ec14eb0a2306e630e58053f1bb264cae9068e443
Aniket Sengar <75060938+Anklet9@users.noreply.github.com>
2023-07-12 22:47:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -77,3 +77,11 @@ function scrollProjects(direction) {
   }
   projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
 }
+let body = document.querySelectorAll('body');
+let toggle = document.getElementById("toggle");
+toggle.onclick = function(){
+  toggle.classList.toggle('active');
+  body.classList.toggle('active');
+}
+
+
